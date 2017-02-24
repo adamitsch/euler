a1ec2c4158d87ea99a35aa119e4979d12e7964dc
adamitsch <jan.adamic@gmail.com>
2017-02-24 21:47:46 +0000

    dodan euler1

---
+++ euler1.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+int main() {
+	int vsota=0;
+	for(int i=3; i<=1000; i++){
+		if( (i%3==0) || (i%5==0)){
+			vsota=vsota+i;
+		}
+	}
+	printf("Vsota je %i.\n",vsota);
+	return 0;
+}
